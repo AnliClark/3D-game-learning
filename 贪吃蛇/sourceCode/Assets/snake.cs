c3d7dbbcafbd5adcad0f8b0fb41735ace1f71855
AnliClark <le.clark9999@Gmail.com>
2024-10-13 23:21:24 +0800

    新版本的贪吃蛇，基本完成

--- 贪吃蛇/sourceCode/Assets/snake.cs
+++ 贪吃蛇/sourceCode/Assets/snake.cs
@@ -5,12 +5,16 @@ using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
 using UnityEngine.UIElements;
+using static UnityEditor.PlayerSettings;
 using Random = System.Random;
 
 public class snake : MonoBehaviour
 {
+    // **********************************//
+    // Entities and their states / Model //
+    // **********************************//
     private int score;
-    private int[,] pos;  // 0表示空，1表示蛇身，2表示奖励
+    private int[,] pos;  // 0表示空，1表示蛇身，2表示蛇头，3表示奖励
     private LinkedList<Tuple<int, int>> snakeBody;  // 存储蛇身的坐标
     private int direction;
     private float timer = 0.0f;
@@ -25,13 +29,15 @@ public class snake : MonoBehaviour
     // 资源
     public Texture boxTexture;
     public Texture snakeTexture;
+    public Texture snakeHeadTexture;
     public Texture appleTexture;
     public Texture arrowUp;
     public Texture arrowDown;
     public Texture arrowleft;
     public Texture arrowRight;
     public Texture restart;
-    private GUIStyle style;
+    private GUIStyle textStyle;
+    private GUIStyle scoreStyle;
 
 
 
@@ -39,48 +45,51 @@ public class snake : MonoBehaviour
     {
         init();
     }
-
+    // **********************************//
+    // View render entities / models     //
+    // **********************************//
     void OnGUI()
     {
         GUI.Box(new Rect(0, 0, windowWidth, windowHeight), "");
-        
-        for(int i = 0; i < squareCount; i++)
+        // 绘制背景图
+        GUI.DrawTexture(new Rect(20, 20, squareWidth*squareCount, squareWidth*squareCount), boxTexture);
+        for (int i = 0; i < squareCount; i++)
         {
-            for(int j = 0; j < squareCount; j++)
+            for (int j = 0; j < squareCount; j++)
             {
-                if (pos[i,j] == 0)
+                if (pos[i, j] == 1)
                 {
-                    GUI.Box(new Rect(20 + i * squareWidth, 20 + j * squareWidth, squareWidth, squareWidth), boxTexture);
-                }else if (pos[i,j] == 1)
-                {
-                    GUI.Box(new Rect(20 + i * squareWidth, 20 + j * squareWidth, squareWidth, squareWidth), snakeTexture);
+                    GUI.DrawTexture(new Rect(20 + i * squareWidth, 20 + j * squareWidth, squareWidth, squareWidth), snakeTexture);
                 }
-                else
+                else if (pos[i, j] == 2)
                 {
-                    GUI.Box(new Rect(20 + i * squareWidth, 20 + j * squareWidth, squareWidth, squareWidth), appleTexture);
+                    GUI.DrawTexture(new Rect(20 + i * squareWidth, 20 + j * squareWidth, squareWidth, squareWidth), snakeHeadTexture);
+                }else if (pos[i, j] == 3)
+                {
+                    GUI.DrawTexture(new Rect(20 + i * squareWidth, 20 + j * squareWidth, squareWidth, squareWidth), appleTexture);
                 }
-                
+
             }
         }
+        GUI.Box(new Rect(windowWidth - 20 - buttonWidth*2, 20, buttonWidth*2, buttonWidth*2), "score:" + score, scoreStyle);
 
-        
         if (GUI.Button(new Rect(140 + squareCount * squareWidth, windowWidth / 4, buttonWidth, buttonWidth), arrowleft))
         {
-            direction = 2;
+            changeDirection(2);
         }// 左
-        else if(GUI.Button(new Rect(160 + squareCount * squareWidth + buttonWidth, windowWidth / 4, buttonWidth, buttonWidth), arrowDown))
+        else if (GUI.Button(new Rect(160 + squareCount * squareWidth + buttonWidth, windowWidth / 4, buttonWidth, buttonWidth), arrowDown))
         {
-            direction = 1;
+            changeDirection(1);
         } // 下
-        else if(GUI.Button(new Rect(160 + squareCount * squareWidth + buttonWidth, windowWidth / 4 - buttonWidth - 20, buttonWidth, buttonWidth), arrowUp))
+        else if (GUI.Button(new Rect(160 + squareCount * squareWidth + buttonWidth, windowWidth / 4 - buttonWidth - 20, buttonWidth, buttonWidth), arrowUp))
         {
-            direction = 0;
+            changeDirection(0);
         } // 上
-        else if(GUI.Button(new Rect(180 + squareCount * squareWidth + buttonWidth * 2, windowWidth / 4, buttonWidth, buttonWidth), arrowRight))
+        else if (GUI.Button(new Rect(180 + squareCount * squareWidth + buttonWidth * 2, windowWidth / 4, buttonWidth, buttonWidth), arrowRight))
         {
-             direction = 3;
+            changeDirection(3);
         } // 右
-        else if(GUI.Button(new Rect(windowWidth - 20 - buttonWidth, windowHeight - 20 - buttonWidth, buttonWidth, buttonWidth), restart))
+        else if (GUI.Button(new Rect(windowWidth - 20 - buttonWidth, windowHeight - 20 - buttonWidth, buttonWidth, buttonWidth), restart))
         {
             init();
             return;
@@ -88,7 +97,7 @@ public class snake : MonoBehaviour
 
         if (lose)
         {
-            GUI.Box(new Rect(windowWidth / 4, windowHeight / 4, buttonWidth, buttonWidth), "You Lose", style);
+            GUI.Box(new Rect(windowWidth / 4, windowHeight / 4, buttonWidth, buttonWidth), "You Lose", textStyle);
             return;
         }
         timer += Time.deltaTime;
@@ -101,36 +110,45 @@ public class snake : MonoBehaviour
             timer = 0.0f;
         }
     }
-
+    // **********************************//
+    // Components /controls              //
+    // **********************************//
     void init()
     {
         score = 0;
         direction = 0;
         lose = false;
         pos = new int[squareCount, squareCount];
-           
+
 
         snakeBody = new LinkedList<Tuple<int, int>>();
-        for (int i=0; i<squareCount; i++)
+        for (int i = 0; i < squareCount; i++)
         {
-            for(int j = 0; j<squareCount; j++)
+            for (int j = 0; j < squareCount; j++)
             {
-                pos[i,j] = 0;
+                pos[i, j] = 0;
             }
         }
-        pos[10, 10] = 1;
+        pos[10, 10] = 2;
         pos[10, 11] = 1;
+        pos[10, 12] = 1;
 
         snakeBody.AddFirst(Tuple.Create(10, 10));
         snakeBody.AddLast(Tuple.Create(10, 11));
+        snakeBody.AddLast(Tuple.Create(10, 12));
+        snakeHeadTexture = Resources.Load<Texture>("蛇头上");
 
         windowWidth = Screen.width - 10;
         windowHeight = Screen.height - 10;
         squareWidth = (windowHeight - 40) / squareCount;
         buttonWidth = windowHeight / 8;
-        style = new GUIStyle();
-        style.fontSize = buttonWidth;
-        style.normal.textColor = Color.blue;
+
+        textStyle = new GUIStyle();
+        textStyle.fontSize = buttonWidth;
+        textStyle.normal.textColor = new Color(190f/255, 74f/255, 47f / 255);
+        scoreStyle = new GUIStyle();
+        scoreStyle.fontSize = buttonWidth/2;
+        scoreStyle.normal.textColor = new Color(190f / 255,74f / 255,47f / 255 );
 
         generateApple();
     }
@@ -139,13 +157,13 @@ public class snake : MonoBehaviour
     {
         int x;
         int y;
-        while (true) 
+        while (true)
         {
             x = rd.Next() % squareCount;
             y = rd.Next() % squareCount;
-            if (pos[x,y] == 0)
+            if (pos[x, y] == 0)
             {
-                pos[x,y] = 2;
+                pos[x, y] = 3;
                 break;
             }
         }
@@ -160,27 +178,32 @@ public class snake : MonoBehaviour
         {
             case 0:
                 x = snakeBody.First.Value.Item1;
-                y = snakeBody.First.Value.Item2 - 1;            
+                y = snakeBody.First.Value.Item2 - 1;
+                snakeHeadTexture = Resources.Load<Texture>("蛇头上");
                 break;
             case 1:
                 x = snakeBody.First.Value.Item1;
-                y = snakeBody.First.Value.Item2 + 1;               
+                y = snakeBody.First.Value.Item2 + 1;
+                snakeHeadTexture = Resources.Load<Texture>("蛇头下");
                 break;
             case 2:
                 x = snakeBody.First.Value.Item1 - 1;
                 y = snakeBody.First.Value.Item2;
+                snakeHeadTexture = Resources.Load<Texture>("蛇头左");
                 break;
             case 3:
                 x = snakeBody.First.Value.Item1 + 1;
                 y = snakeBody.First.Value.Item2;
+                snakeHeadTexture = Resources.Load<Texture>("蛇头右");
                 break;
         }
-        if(collision(x, y) == 0)
+        if (collision(x, y) == 0)
         {
             // 删去尾部
             pos[snakeBody.Last.Value.Item1, snakeBody.Last.Value.Item2] = 0;
             snakeBody.RemoveLast();
-        }else if(collision(x, y) == 1)
+        }
+        else if (collision(x, y) == 1)
         {
             return false;
         }
@@ -189,8 +212,9 @@ public class snake : MonoBehaviour
             score++;
             generateApple();
         }
-        snakeBody.AddFirst(Tuple.Create(x, y));
         pos[snakeBody.First.Value.Item1, snakeBody.First.Value.Item2] = 1;
+        snakeBody.AddFirst(Tuple.Create(x, y));
+        pos[x, y] = 2;
         return true;
     }
 
@@ -209,10 +233,14 @@ public class snake : MonoBehaviour
         {
             return 0;
         }
-        else if (pos[x, y] == 1)
+        else if (pos[x, y] == 1 || pos[x, y] == 2)
         {
             return 1;
         }
         else { return 2; }
+    }
+    void changeDirection(int x)
+    {
+        direction = x;
     }
 }
